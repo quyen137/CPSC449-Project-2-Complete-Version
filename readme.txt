52ecee29fb00f26be5e486b571dc2a54874ec705
student <quyenhua137@gmail.com>
2020-05-04 18:30:04 -0700

    final working version

--- readme.txt
+++ readme.txt
@@ -1,125 +1,47 @@
 Team member         CWID#                 Role
-Alex Tran           891297442             Ops
-Tu Tran                                   Dev (Posting Microservices)
-Joseph Hoang                              Dev (Voting Microservices)
+Alex Tran          91297442    		Dev (Backend for Frontend)
+Tu Tran            888750130		Dev (Posting Microservices)
+Joseph Hoang       889782900		Dev (Voting Microservices)
+
 
 Project Overview
-	The scope of this project is to be able to implement two Microservices: Posting, Voting.
-	We will also need to be able to run multiple instances of each Microservices while using
-	load balancer to divide up the requests from each instance.
+
+Dev 3: Aggregating posts and votes with a BFF
+	By using BFF pattern, this microservices will be pulling data from the other services and expose it in a format of RSS that can be used directly by the frontend 
+	For this project, each microservices will be given a default port to ensure that all microservices will work properly all
+	Posting microservices: localhost:5000/post/posts/all 	
+	Voting microservices: localhost:5100/api/v1/resources/votes/all 
+	BFF microservices: localhost:5200/recent
+
     
 
 Instructions to run
-  	These are the library, and tools that will need to be install in order to run the project
+Install Flask 
+	$ pip3 install --user Flask-API python-dotenv
+Install Request 
+	$ pip3 install --user requests
+Install foreman & Gunicorn3
+	$ sudo apt install --yes ruby-foreman
+	$ sudo apt install --yes gunicorn3
+Install Feed Generator
+	$ sudo apt update
+	$ sudo apt install --yes python3-lxml
+	$ pip3 install --user feedgen
 
-		pip3 install --user Flask-API python-dotenv
-		pip3 install --user pugsql 
-		sudo apt install --yes ruby-foreman
-    
-   	First, we need to init the database for the both Posting and Voting Microservices
-	If you can't run flask init, please check Flask Error section.
-	
-		flask init
+RSS feeds provided by the BFF Microservices
 
-	If you only run one instance per Microservices, please refresh  multiple times.
-   	Because of load balancer, it will take 1-2 to get to the correct $PORT
-	Then we will start the project by running:
-
-		foreman start
-
-	Since, this the project required starting 3 instances of each Microservices.
-	Port number already been hardcoded in to work with only 3 or less instances.
-	If you want to run multiple instances,please use this link to run the project:
-
-		foreman start -m web1=<number of instances => 3>,web2=<number of instances => 3>,caddy=1
-
-Flask Error
-	If you get Error: No such command init, please create .env file
-	.env will contain the following {
-		FLASK_APP=vote
-		FLASK_APP=api
-		FLASK_ENV=development
-		APP_CONFIG=api.cfg
-	}
-Port In Use Error 
-	sudo lsof -i -P -n | grep LISTEN
-	kill <port id>
+The 25 most recent posts to any community
+	http://localhost:5200/recent
+The 25 most recent posts to a particular community
+	http://localhost:5200/<string>/<int> 
+	http://localhost:5200/school/25 
+Top 25 posts to any community, sorted by score
+	http://localhost:5200/score 
+The top 25 posts to a particular community, sorted by score
+	http://localhost:5200/score/<string>
+	http://localhost:5200/score/school
+The hot 25 posts to any community, ranked using Reddit hot ranking algorithm
+	http://localhost:5200/hot 
 
 
-Developer Ops
-	For Ops role, I will be using Caddy for webserver and Gunicorn for WSGI server
-	Two host open for each Microservices.
-
-	User will be able to access to Post or Vote Microservices by using these hosts:
-
-		localhost:2015/post
-		localhost:2015/vote
-
-	Load Balancer
-		Round Robin methods have been implemented for load balancing requests
-		--access-logfile - also have been implemented for testing of load balancing requests in Procfile
-		
-
-Posting Microservices
-	For Posting Microservices, below is the following route to specific functions
-
-	To view home page for post:
-		REQUEST GET 
-		localhost:2015/post
-
-	To view all posts:
-		REQUEST GET 
-		localhost:2015/post/posts/all	
-
-	To view a specific post: 
-		REQUEST GET 
-		localhost:2015/post/posts/<id>
-		example: localhost:2015/post/posts/1
-
-	To delete a specific post: 
-		REQUEST GET, DELETE
-		localhost:2015/post/posts/delete/<id>
-		example: localhost:2015/post/posts/delete/1
-
-	To create: 
-	json format is required, all fields are required, 
-		REQUEST GET, POST
-		localhost:2015/post/posts
-		example: 
-			{
-			    "Username": "User 1",
-			    "PostTitle": "Post Title 1",
-			    // "PostDate": "01/01/2020",
-			    "Content": "Content 1",
-			    "Community": "Community 1",
-			    "URLResource": "www.URLResource1.com"
-			}
-
-	To view a specific community in posts:
-		REQUEST GET 
-		http://localhost:5000/posts/<community name>
-		example: localhost:2015/post/posts/Community_1
-    
-Voting Microservices
-	View all votes: http://127.0.0.1:5000/api/v1/resources/votes/all
-
-	View votes by vote id: http://127.0.0.1:5000/api/v1/resources/votebyid/1
-
-	Upvote a post: http://127.0.0.1:5000/api/v1/resources/upvote
-		Have to input in json format. 
-		Example {“postID”: 10}
-
-	Downvote a post: http://127.0.0.1:5000/api/v1/resources/downvote
-		Have to input in json format. 
-		Example {“postID”: 11}
-
-	Report the number of upvotes and downvotes for a post:
-		http://127.0.0.1:5000/api/v1/resources/votesbypostid/101
-		
-	List the n top-scoring posts to any community:
-		http://127.0.0.1:5000/api/v1/resources/toppostscore/3
-
-	Given a list of post identifiers, return the list sorted by score:
-		http://127.0.0.1:5000/api/v1/resources/listsortedbyscore
-		Have to input a list: Example: [10, 13, 15]
 
--- rss.py
+++ rss.py
@@ -36,7 +36,6 @@ def posts_by_specific_community(Community,n):
 @app.route('/recent')
 def posts_by_community():
     URL = "http://localhost:5000/posts?n=25"
-    print(URL)
     result = requests.get(URL)
     jsonResponse = result.json()
     fg = FeedGenerator()
@@ -62,47 +61,50 @@ def posts_by_community():
 # The top 25 posts to a particular community, sorted by score
 @app.route('/score/<string:Community>')
 def score_by_community(Community):
-    print("community: ", Community)
-    URL = r"http://localhost:5000/posts/" + f'{Community}' + r"/25"
-    print (URL)
-    vote = "http://localhost:5100/api/v1/resources/votes/all"
-    resultURL = requests.get(URL)
-    resultVote = requests.get(vote)
-    jsonResponse = resultURL.json()
-    jsonVoteResponse = resultVote.json()
-    sort_obj = sorted(jsonVoteResponse,key=lambda x : x ['upVoted'], reverse=True)
-    # print("Entire JSON response")
+    post = r"http://localhost:5000/posts/" + f'{Community}' + r"/25"
+    resultPost = requests.get(post)
+    jsonResponse = resultPost.json()
+    mylist = []
+    for each in jsonResponse:
+        vote = r"http://127.0.0.1:5100/vote/" + f'{each["PostID"]}'
+        queryVote = requests.get(vote)
+        jsonResponseVote = queryVote.json()
+        score = jsonResponseVote['upvote'] - jsonResponseVote['downvote']
+        mylist.append({'ID': each['PostID'], 'score':score})
+    sort_obj = sorted(mylist,key=lambda x : x ['score'], reverse=True)
     fg = FeedGenerator()
     fg.id('http://localhost:5200/score/<string:Community>')
     fg.title('The top 25 posts to a particular community, sorted by score')
     fg.link( href='http://localhost:5200/score/<string:Community>',rel='alternate')
     fg.description('Printing specific community by score')
     fg.language('en')
-    i = 0
-    for data in sort_obj:
+    temp = 0
+    for obj in sort_obj:
         for each in jsonResponse:
-            if (data['postID'] == each['PostID']):
+            if temp == 25:
+                break
+            if (each["PostID"] == obj["ID"]):
                 fe = fg.add_entry()
                 fe.id(str(each["PostID"]))
                 fe.title(each["PostTitle"])
                 fe.author({'name':each["Username"], 'email':'default@csu.fullerton.edu'} )
                 fe.link( href=each["URLResource"], rel='alternate' )
                 fe.content(each["Community"])
-                i+=1
+                temp+=1
                 break
-    rssfeed  = fg.rss_str(pretty=True) 
-    return rssfeed
+    rssfeed  = fg.rss_str(pretty=True)
+    return rssfeed  
 
 # Top 25 posts to any community, sorted by score
 @app.route('/score')
 def posts_by_score_community():
-    vote = "http://localhost:5100/api/v1/resources/votes/all"
+    vote = "http://localhost:5100/toppostscore/25"
     post = "http://localhost:5000/posts/all"
     queryVote = requests.get(vote)
     queryPost = requests.get(post)
     jsonResponseVote = queryVote.json()
     jsonResponsePost = queryPost.json()
-    sort_obj = sorted(jsonResponseVote,key=lambda x : x ['upVoted'], reverse=True)
+    # sort_obj = sorted(jsonResponseVote,key=lambda x : x ['upVoted'], reverse=True)
     fg = FeedGenerator()
     fg.id('http://localhost:5200/score')
     fg.title('Top 25 posts to any community, sorted by score')
@@ -110,7 +112,7 @@ def posts_by_score_community():
     fg.description('Printing any community by score')
     fg.language('en')
     i = 0
-    for each in sort_obj:
+    for each in jsonResponseVote:
         for post in jsonResponsePost:
             if each['postID'] == post['PostID']:
                 if (i == 25):
@@ -131,29 +133,27 @@ def posts_by_score_community():
 # The hot 25 posts to any community, ranked using Reddit's "hot ranking" algorithm
 @app.route('/hot')
 def hot_post():
-    vote = "http://localhost:5100/api/v1/resources/votes/all"
-    queryVote = requests.get(vote)
-    jsonResponseVote = queryVote.json()
-    mylist = []
-    i = 0
-    for each in jsonResponseVote:
-        if i == 100:
-            break
-        value = hotPost(each['upVoted'],each['downVoted'],epoch_seconds(each['postID']))
-        mylist.append({'ID': each['postID'], 'score':value})
-        i+=1
-    sort_obj = sorted(mylist,key=lambda x : x ['score'], reverse=True)
-    print (sort_obj)
     post = "http://localhost:5000/posts/all"
     queryPost = requests.get(post)
     jsonResponsePost = queryPost.json()
-    temp = 0
+    mylist = []
+    for each in jsonResponsePost:
+        second = epoch_seconds(each['PostDate'])
+        vote = r"http://127.0.0.1:5100/vote/" + f'{each["PostID"]}'
+        queryVote = requests.get(vote)
+        jsonResponseVote = queryVote.json()
+        result = hotPost(jsonResponseVote['upvote'],jsonResponseVote['downvote'],second)
+        mylist.append({'ID': each['PostID'], 'score':result})
+    sort_obj = sorted(mylist,key=lambda x : x ['score'], reverse=True)
+    print(mylist,"\n")
+    print(sort_obj, "\n")
     fg = FeedGenerator()
     fg.id('http://localhost:5200/hot')
     fg.title('The hot 25 posts to any community, ranked using Reddit hot ranking algorithm')
     fg.link( href='http://localhost:5200/hot',rel='alternate')
     fg.description('Printing out 25 Hot Trending Posts')
     fg.language('en')
+    temp = 0
     for obj in sort_obj:
         for post in jsonResponsePost:
             if temp == 25:
@@ -165,23 +165,22 @@ def hot_post():
                 fe.author({'name':post["Username"], 'email':'default@csu.fullerton.edu'} )
                 fe.link( href=post["URLResource"], rel='alternate' )
                 fe.description(post["Community"])
+                temp +=1
+                break
     rssfeed  = fg.rss_str(pretty=True)
     return rssfeed  
 
-def epoch_seconds(postID):
-    post = "http://localhost:5000/posts/all"
-    queryPost = requests.get(post)
-    jsonResponsePost = queryPost.json()
-    for each in jsonResponsePost:
-        if (postID == each['PostID']):
-            format = "%Y-%m-%d %H:%M:%S"
-            date = datetime.strptime(each['PostDate'],format)   
-            value = date.timestamp()
-            return (value)
+
+def epoch_seconds(PostDate):
+    temp = PostDate.split('.')[0]
+    format = "%Y-%m-%d %H:%M:%S"
+    date = datetime.strptime(temp,format)   
+    value = date.timestamp()
+    return (value)
 
 def hotPost(upVoted, downVoted, date):
     score = int(upVoted) - int(downVoted)
     order = log(max(abs(score), 1), 10)
     sign = 1 if score > 0 else -1 if score < 0 else 0
-    seconds =  date - 1134028003
+    seconds =  date - 1134028003.0
     return (round(sign * order + seconds / 45000, 7))
